cc838639a00e151cf1e9a9c036424bf4101b9109
TheValll <valentin78.massonniere@gmail.com>
2025-04-07 16:54:37 +0200

    java

--- tests/appli/src/main/java/appli/Calculator.java
+++ tests/appli/src/main/java/appli/Calculator.java
@@ -5,43 +5,51 @@ public class Calculator implements  ICalculator {
     @Override
     public int multiply(int a, int b) {
         int result = 0;
-        for (int i = 0; i < b; i++) {
-            result = add(result, a);
+        int absA = Math.abs(a);
+        int absB = Math.abs(b);
+
+        for (int i = 0; i < absB; i++) {
+            result += absA;
         }
+
+        if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+            result = -result;
+        }
+
         return result;
     }
 
     @Override
     public int divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Division by zero is not allowed.");
-        }
-        int result = 0;
-        int sum = 0;
-        while (sum < a) {
-            sum = add(sum, b);
-            if (sum <= a) {
+        return a / b;
+    }
+
+    @Override
+    public int add(int a, int b) {
+        int result = a;
+        int absB = Math.abs(b);
+
+        for (int i = 0; i < absB; i++) {
+            if (b > 0) {
                 result++;
+            } else {
+                result--;
             }
         }
         return result;
     }
 
     @Override
-    public int add(int a, int b) {
-        for(int i =0; i<a; i++) {
-            b++;
-        }
-
-        return b;
-    }
-
-    @Override
     public int substract(int a, int b) {
-        for(int i =0; i<a; i++) {
-            b--;
+        int result = a;
+        int absB = Math.abs(b);
+        for (int i = 0; i < absB; i++) {
+            if (b > 0) {
+                result--;
+            } else {
+                result++;
+            }
         }
-
-        return b;
+        return result;
     }
 }
--- tests/appli/src/test/java/appli/CalculatorTest.java
+++ tests/appli/src/test/java/appli/CalculatorTest.java
@@ -1,77 +1,9 @@
 package appli;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+
 public class CalculatorTest {
-
-    @Test
-    public void multiplyABpositif() {
-        Calculator calc = new Calculator();
-        int a = 1;
-        int b = 2;
-        int result = a*b;
-
-        if (calc.multiply(a, b) == result) {
-            System.out.println("a and b positif");
-        }
-    }
-
-    @Test
-    public void multiplyApositifBnegitif() {
-        Calculator calc = new Calculator();
-        int a = 1;
-        int b = -2;
-        int result = a*b;
-        if (calc.multiply(a, b) == result) {
-            System.out.println("a positif and b negitif");
-        }
-    }
-
-    @Test
-    public void multiplyABnegitif() {
-        Calculator calc = new Calculator();
-        int a = -1;
-        int b = -2;
-        int result = a*b;
-        if (calc.multiply(a, b) == result) {
-            System.out.println("a and b negitif");
-        }
-    }
-
-    @Test
-    public void divideABpositif() {
-        Calculator calc = new Calculator();
-        int a = 1;
-        int b = 2;
-        int result = a/b;
-
-        if (calc.divide(a, b) == result) {
-            System.out.println("a and b positif");
-        }
-    }
-
-    @Test
-    public void divideApositifBnegitif() {
-        Calculator calc = new Calculator();
-        int a = 1;
-        int b = -2;
-        int result = a/b;
-        if (calc.divide(a, b) == result) {
-            System.out.println("a positif and b negitif");
-        }
-    }
-
-    @Test
-    public void divideABnegitif() {
-        Calculator calc = new Calculator();
-        int a = -1;
-        int b = -2;
-        int result = a/b;
-        if (calc.divide(a, b) == result) {
-            System.out.println("a and b negitif");
-        }
-    }
 
     @Test
     public void addABpositif() {
@@ -80,68 +12,176 @@ public class CalculatorTest {
         int b = 2;
         int result = a + b;
 
-        if (calc.add(a, b) == result) {
-            System.out.println("a and b positif");
+        if (calc.add(a, b) != result) {
+            fail("add failed a b positif");
         }
     }
 
     @Test
-    public void addApositifBNegatif() {
-        Calculator calc = new Calculator();
-        int a = 1;
-        int b = -2;
-        int result = a + b;
-
-        if (calc.add(a, b) == result) {
-            System.out.println("a positif and b negatif");
-        }
-    }
-
-    @Test
-    public void addABNegatif() {
+    public void addABnegitif() {
         Calculator calc = new Calculator();
         int a = -1;
         int b = -2;
         int result = a + b;
-
-        if (calc.add(a, b) == result) {
-            System.out.println("a and b negatif");
+        if (calc.add(a, b) != result) {
+            fail("add failed a b negitif");
         }
     }
 
+    @Test
+    public void addApositifBnegitif(){
+        Calculator calc = new Calculator();
+        int a = 1;
+        int b = -2;
+        int result = a + b;
+        if (calc.add(a, b) != result) {
+            fail("add failed a positif b negitif");
+        }
+    }
+
+    @Test
+    public void addAnegatifBpositif(){
+        Calculator calc = new Calculator();
+        int a = -1;
+        int b = 2;
+        int result = a + b;
+        if (calc.add(a, b) != result) {
+            fail("add failed a positif b negitif");
+        }
+    }
 
     @Test
     public void substractABpositif() {
         Calculator calc = new Calculator();
-        int a = 1;
-        int b = 2;
+        int a = 2;
+        int b = 1;
         int result = a - b;
 
-        if (calc.substract(a, b) == result) {
-            System.out.println("a and b positif");
+        if (calc.substract(a, b) != result) {
+            fail("substract failed a b positif");
         }
     }
 
     @Test
-    public void substractApositifBnegatif() {
+    public void substractABnegitif() {
         Calculator calc = new Calculator();
-        int a = 1;
-        int b = -2;
+        int a = -2;
+        int b = -1;
         int result = a - b;
-
-        if (calc.substract(a, b) == result) {
-            System.out.println("a and b negatif");
+        if (calc.substract(a, b) != result) {
+            fail("substract failed a b negitif");
         }
     }
 
     @Test
-    public void substractABNegatif() {
+    public void substractApositifBnegitif(){
         Calculator calc = new Calculator();
-        int a = -1;
-        int b = -2;
+        int a = 2;
+        int b = -1;
         int result = a - b;
-        if (calc.substract(a, b) == result) {
-            System.out.println("a and b negatif ");
+        if (calc.substract(a, b) != result) {
+            fail("substract failed a positif b negitif");
+        }
+    }
+
+    @Test
+    public void substractAnegatifBpositif(){
+        Calculator calc = new Calculator();
+        int a = -2;
+        int b = 1;
+        int result = a - b;
+        if (calc.substract(a, b) != result) {
+            fail("substract failed a negatif b positif");
+        }
+    }
+
+    @Test
+    public void multiplyABpositif() {
+        Calculator calc = new Calculator();
+        int a = 2;
+        int b = 3;
+        int result = a * b;
+
+        if (calc.multiply(a, b) != result) {
+            fail("multiply failed a b positif");
+        }
+    }
+
+    @Test
+    public void multiplyABnegitif() {
+        Calculator calc = new Calculator();
+        int a = -2;
+        int b = -3;
+        int result = a * b;
+        if (calc.multiply(a, b) != result) {
+            fail("multiply failed a b negitif");
+        }
+    }
+
+    @Test
+    public void multiplyApositifBnegitif(){
+        Calculator calc = new Calculator();
+        int a = 2;
+        int b = -3;
+        int result = a * b;
+        if (calc.multiply(a, b) != result) {
+            fail("multiply failed a positif b negitif");
+        }
+    }
+
+    @Test
+    public void multiplyAnegatifBpositif(){
+        Calculator calc = new Calculator();
+        int a = -2;
+        int b = 3;
+        int result = a * b;
+        if (calc.multiply(a, b) != result) {
+            fail("multiply failed a negatif b positif");
+        }
+    }
+
+    @Test
+    public void divideABpositif() {
+        Calculator calc = new Calculator();
+        int a = 6;
+        int b = 3;
+        int result = a / b;
+
+        if (calc.divide(a, b) != result) {
+            fail("divide failed a b positif");
+        }
+    }
+
+    @Test
+    public void divideABnegitif() {
+        Calculator calc = new Calculator();
+        int a = -6;
+        int b = -3;
+        int result = a / b;
+        if (calc.divide(a, b) != result) {
+            fail("divide failed a b negitif");
+        }
+    }
+
+    @Test
+    public void divideApositifBnegitif(){
+        Calculator calc = new Calculator();
+        int a = 6;
+        int b = -3;
+        int result = a / b;
+        if (calc.divide(a, b) != result) {
+            fail("divide failed a positif b negitif");
+        }
+    }
+
+    @Test
+    public void divideAnegatifBpositif(){
+        Calculator calc = new Calculator();
+        int a = -6;
+        int b = 3;
+        int result = a / b;
+        if (calc.divide(a, b) != result) {
+            fail("divide failed a negatif b positif");
         }
     }
 }
